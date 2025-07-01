5e64cb8418a9b86bb48acc545d48941ddda78121
Oleksandr Tymoshchuk <o.timoshuk@gmail.com>
2025-07-01 16:52:31 +0000

    Add Micropost Form.

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -3,13 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\MicroPost;
+use App\Form\MicroPostTypeForm;
 use App\Repository\MicroPostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class MicroPostController extends AbstractController
 {
@@ -32,11 +31,7 @@ class MicroPostController extends AbstractController
     #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
     public function add(Request $request, MicroPostRepository $posts): Response
     {
-      $microPost = new MicroPost();
-      $form = $this->createFormBuilder($microPost)
-        ->add('title')
-        ->add('text')
-        ->getForm();
+      $form = $this->createForm(MicroPostTypeForm::class, new MicroPost());
 
       $form->handleRequest($request);
 
@@ -62,10 +57,7 @@ class MicroPostController extends AbstractController
   #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')]
   public function edit(MicroPost $post,Request $request, MicroPostRepository $posts): Response
   {
-    $form = $this->createFormBuilder($post)
-      ->add('title')
-      ->add('text')
-      ->getForm();
+    $form = $this->createForm(MicroPostTypeForm::class, $post);
 
     $form->handleRequest($request);
 
