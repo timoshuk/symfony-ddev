2711085109570de2907d0913fc5e2648ae085fbd
Oleksandr Tymoshchuk <o.timoshuk@gmail.com>
2025-07-06 09:54:40 +0000

    add related entities.

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Entity\UserProfile;
+use App\Repository\UserProfileRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HelloController extends AbstractController {
   private array $messages = [
@@ -12,13 +16,27 @@ class HelloController extends AbstractController {
     ['message' => 'Hi', 'created' => '2023/08/12'],
     ['message' => 'Bye!', 'created' => '2022/10/12']
   ];
-  #[Route('/{limit<\d+>?3}', name: 'app_index')]
-  public function index(int $limit): Response {
+  #[Route('/', name: 'app_index')]
+  public function index(UserProfileRepository $profiles, EntityManagerInterface $entityManager): Response {
+//    $user = new User();
+//    $user->setEmail('email@test.com');
+//    $user->setPassword('12345678');
+//
+//    $profile = new UserProfile();
+//    $profile->setUser($user);
+//    $entityManager->persist($profile);
+//    $entityManager->flush();
+
+
+      $profile = $profiles->find(2);
+      $entityManager->remove($profile);
+      $entityManager->flush();
+
     return $this->render(
       'hello/index.html.twig',
       [
         'messages' => $this->messages,
-        'limit' => $limit
+        'limit' => 3
       ]
     );
   }
